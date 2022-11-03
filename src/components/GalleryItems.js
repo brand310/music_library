036dbab202d33e7557e5c89182b04df2a07ea990
brand310 <brandonsandoval0@gmail.com>
2022-11-03 16:45:11 -0700

    Added react-router

--- src/components/GalleryItems.js
+++ src/components/GalleryItems.js
@@ -1,23 +1,54 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export function GalleryItem({ track }) {
   let [isExpanded, setIsExpanded] = useState(false);
 
   const clickHandler = () => {
     setIsExpanded(!isExpanded);
   };
-  // styles ---------------------------------------
+  // styles --------------------------------------------
 
-  // views ----------------------------------------
+  const simpleStyle = {
+    width: "25vw",
+    height: "20vh",
+    border: "1px solid black",
+    margin: "2px",
+  };
+
+  const detailStyle = {
+    width: "80vw",
+    height: "20vh",
+    border: "1px solid black",
+    margin: "2px",
+    backgroundImage: `url(${track.artworkUrl100})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    color: "yellow",
+  };
+
+  // views ---------------------------------------------
+
   const simpleView = (
-    <div>
+    <div style={simpleStyle}>
       <h3>{track.trackName}</h3>
       <h4>{track.collectionName}</h4>
+      <img src={track.artworkUrl100} alt="album art" />
     </div>
   );
+
   const detailedView = (
-    <div>
+    <div style={detailStyle}>
       <h2>{track.trackName}</h2>
-      <h3>{track.collectionName}</h3>
+
+      <h3>
+        <Link to={`/artist/${track.artistId}`}>{track.artistName}</Link>
+      </h3>
+
+      <h3>
+        <Link to={`/album/${track.collectionId}`}>{track.collectionName}</Link>
+      </h3>
+
       <h4>{track.primaryGenreName}</h4>
       <h4>{track.releaseDate}</h4>
     </div>
@@ -25,10 +56,7 @@ export function GalleryItem({ track }) {
 
   return (
     <div onClick={clickHandler} style={{ display: "inline-block" }}>
-      {/* This simple ternary shows the simple view when 'view' is false! */}
       {isExpanded ? detailedView : simpleView}
     </div>
   );
 }
-
-export default GalleryItem;
